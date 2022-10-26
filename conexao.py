979f5dc496cce1e5ef3a4919f0b59cd940cc433f
Ian Vinícius Vasconcelos <81888827+NerdAleatorio@users.noreply.github.com>
2022-10-26 01:38:27 +0000

    Initial commit

---
+++ conexao.py
@@ -0,0 +1,43 @@
+#Bibliotecas para Banco de Dados
+import pandas as pd
+import sqlite3
+import json
+
+#Criar database
+arquivo = "banco.db"
+
+#Comandos DDL e DML
+def iniciar_conexao():
+          conexao = None
+          try:  
+            conexao = sqlite3.connect(arquivo)
+            print('\033[1;49;32mConexão bem sucedida.\033[m')
+
+          except sqlite3.Error as var:
+            print('\033[1;49;31mConexão mal sucedida.\033[m')
+
+
+def fechar_conexao(conexao):
+          if conexao:
+            conexao.close()
+
+
+def criar_tabela(conexao, SQL_criar_tabela):
+          try:
+            cursor = conexao.cursor()
+            cursor.execute(SQL_criar_tabela)
+            print('\033[1;49;32mTabela criada com sucesso.\033[m')
+
+          except sqlite3.Error as var:
+            print('\033[1;49;31mComando mal sucedido.\033[m')
+
+
+def inserir_usuario(conexao, SQL_inserir_usuario):
+          try:
+            cursor = conexao.cursor()
+            cursor.exceute(SQL_inserir_usuario)
+            print('\033[1;49;32mUsuário inserido com sucesso.\033[m')
+
+          except sqlite3.Error as var:
+            print('\033[1;49;31mComando mal sucedido.\033[m')
+
---
+++ main.py
@@ -0,0 +1,19 @@
+#Bibliotecas
+from flask import Flask, render_template, redirect 
+from flask.globals import request
+from usuario import *
+from conexao import *
+
+
+
+#Código principal
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+          return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=81)
+  
